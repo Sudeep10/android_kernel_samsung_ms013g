a171fb5b0491dc35b2f73f0b6db9a96672f68f89
Sudeep <shettysudeep1010@gmail.com>
2019-03-30 11:47:05 +0000

    Fixing duplicate member error

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -155,7 +155,6 @@ struct scsi_device {
 	unsigned is_visible:1;	/* is the device visible in sysfs */
 	unsigned use_rpm_auto:1; /* Enable runtime PM auto suspend */
 	unsigned broken_fua:1;		/* Don't set FUA bit */
-	unsigned use_rpm_auto:1; /* Enable runtime PM auto suspend */
 
 #define SCSI_DEFAULT_AUTOSUSPEND_DELAY  -1
 	int autosuspend_delay;
